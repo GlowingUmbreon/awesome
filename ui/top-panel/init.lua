c05162a9942b9820fa14c5fb0bee97f26bd8bc0f
Umbreon <umbreon@disroot.org>
2022-02-24 20:51:15 +0000

    MOAR IMPROVEMENTS

--- ui/top-panel/init.lua
+++ ui/top-panel/init.lua
@@ -25,60 +25,71 @@ kbdcfg.widget:buttons(awful.util.table.join(
 systray = wibox.widget.systray()
 systray.visible = false
 
-date = wibox.widget.textclock(" %a %b %d ")
-time = wibox.widget.textclock("%H:%M",30)
+local padding = 12
+local halfPad = 6
+
+local function AddPad(obj)
+	local a = wibox.container.margin(
+		obj,
+		10,
+		10,
+		0,
+		0
+	)
+	a.forced_height = configuration.toppanel_height+5
+	return wibox.container.background(
+		a,
+		dracula.transparentBackground
+	)
+end
+
+date = AddPad(wibox.widget.textclock(" %a %b %d "))
+time = AddPad(wibox.widget.textclock(" %H:%M 3"))
 
 local TopPanel = function(s)
-	-- Wiboxes are much more flexible than wibars simply for the fact that there are no defaults, however if you'd rather have the ease of a wibar you can replace this with the original wibar code
 	local panel = wibox({
 		ontop = true,
 		screen = s,
-		height = configuration.toppanel_height,
-		width = s.geometry.width-10,
-		position = "top",
-		x = s.geometry.x+5,
-		y = s.geometry.y+5,
-		stretch = false,
+		height = configuration.toppanel_height+halfPad,
+		width = s.geometry.width-padding,
+		x = s.geometry.x+halfPad,
+		y = s.geometry.y+halfPad,
 		bg = beautiful.background,
 		fg = beautiful.fg_normal,
-		struts = {
-			top = configuration.toppanel_height,
-		},
 	})
 
 	panel:struts({
-		top = configuration.toppanel_height+10,
+		top = configuration.toppanel_height+padding,
 	})
-	--
 
 	panel:setup({
-		layout = wibox.layout.stack,
-		{
-			layout = wibox.layout.align.horizontal,
-			{ -- Left widgets
-				layout = wibox.layout.fixed.horizontal,
-				--mylauncher,
-				s.mytaglist,
-				s.mypromptbox,
-			},
-			nil,
-			{
-				layout = wibox.layout.fixed.horizontal,
-				systray,
-				date,
-			},
+		{ -- Left widgets
+			layout = wibox.layout.fixed.horizontal,
+			AddPad(s.mytaglist),
+			--s.mypromptbox,
 		},
+		{
+			layout = wibox.layout.ratio.horizontal,
+			wibox.container.place(time, 'center', 'center'),
+		},
+		{
+			layout = wibox.layout.fixed.horizontal,
+			--systray,
+			date,
+		},
+		layout = wibox.layout.align.horizontal,
 		
 		--s.mytasklist, -- Middle widget
 		--s.mylayoutbox,
 		--kbdcfg,
-		{ -- Right widgets
+		--[[{ -- Right widgets
 			--layout = wibox.layout.fixed.horizontal,
 			layout = wibox.container.place,
+			forced_height = 30,
 			valign = "center",
 			halign = "center",
 			time,
-		},
+		},]]
 	})
 
 	return panel
